231c8cd61edb460d0b1574e39e072c1355d6dad4
uyr83157 <uyr83157@gmail.com>
2025-02-14 20:01:12 +0900

    feat: 댓글 엔티티 구현

---
+++ src/main/java/com/example/newsfeed/Entity/Comment.java
@@ -0,0 +1,51 @@
+package com.example.newsfeed.Entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.springframework.data.annotation.LastModifiedBy;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Comment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long commentId;
+
+    // optional = false: 반드시 해당 엔티티와 연관관계를 가지고 있어야됨
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "post_id", nullable = false)
+    private Post post;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "member_id", nullable = false)
+    private Member member;
+
+    // null 가능 => null 이면 최상위 댓글 , null 이 아니면 대댓글
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "parent_comment_id")
+    private Comment parentComment;
+
+    @Column(nullable = false)
+    private String content;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @LastModifiedBy
+    private String modifiedBy;
+
+    // null 이면 삭제 안된 상태 => soft delete 처리
+    private LocalDateTime deletedAt;
+
+
+}
