48ff31105b76e9e4d39ddfc61d9573d5a49aedfc
uyr83157 <uyr83157@gmail.com>
2025-02-18 16:06:35 +0900

    Revert "Juy"

--- src/main/java/com/example/newsfeed/comment/exception/DeletedCommentException.java
+++
@@ -1,7 +0,0 @@
-package com.example.newsfeed.comment.exception;
-
-public class DeletedCommentException extends RuntimeException {
-    public DeletedCommentException(String message) {
-        super(message);
-    }
-}
--- src/main/java/com/example/newsfeed/comment/exception/RepetitionLikeException.java
+++
@@ -1,7 +0,0 @@
-package com.example.newsfeed.comment.exception;
-
-public class RepetitionLikeException extends RuntimeException {
-    public RepetitionLikeException(String message) {
-        super(message);
-    }
-}
